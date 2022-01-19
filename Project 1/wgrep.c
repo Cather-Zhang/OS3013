ac6ead51f8fd1eac78926faa4f09b0b6266227d7
JaiiR320 <jair3202@gmail.com>
2022-01-19 12:20:32 -0500

    Finished wgrep

--- Project 1/wgrep.c
+++ Project 1/wgrep.c
@@ -15,34 +15,36 @@ int main(int argc, char const *argv[]) {
      * 
      */
     if (argc == 2) {
-        printf("Enter here: \n");      
-        char *str = NULL;
+        //printf("Enter here: \n");      
+        char str[256];
         while (fgets(str, 256, stdin) != NULL) {
             c = strstr(str, argv[1]);
             if (c) printf("%s", str);   
         }
         return 0;
     }
-    for (size_t i = 2; i < argc; i++) {
-        //printf("opening file %s\n", argv[i]);
-        
-        FILE *file;
-        file = fopen(argv[i], "r");
-        if (file) {
-            char *line = NULL;
-            size_t len = 0;
-            while(getline(&line, &len, file) != -1) {
-                c = strstr(line, argv[1]);
-                if (c) printf("%s", line);  
+    else {
+        for (size_t i = 2; i < argc; i++) {
+            //printf("opening file %s\n", argv[i]);
+            
+            FILE *file;
+            file = fopen(argv[i], "r");
+            if (file) {
+                char *line = NULL;
+                size_t len = 0;
+                while(getline(&line, &len, file) != -1) {
+                    c = strstr(line, argv[1]);
+                    if (c) printf("%s", line);  
+                }
+                free(line);
+                fclose(file);
             }
-            free(line);
-            fclose(file);
+            else {
+                printf("wgrep: cannot open file\n");
+                return 1;   
+            }
         }
-        else {
-            printf("wgrep: cannot open file\n");
-            return 1;   
-        }
+        return 0;
     }
-    return 0;
 }
 
