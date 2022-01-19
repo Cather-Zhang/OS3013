60e30c8f9d6512ecb3b3bbda4bd23fac418abbc0
Cather-Zhang <cather0215@gmail.com>
2022-01-19 21:30:34 +0800

    stdin not resolved

--- Project 1/wgrep.c
+++ Project 1/wgrep.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-    if (argc == 1) printf("wgrep: searchterm [file ...]");
-    char* str = argv[1];
+    char* c;
+    //printf("found %d arguments\n", argc);
+    if (argc == 1) {
+        printf("wgrep: searchterm [file ...]\n");
+        return 0;
+    }
+    //printf("need to search %s\n", argv[1]);
+    if (argc == 2) {
+        printf("Enter here: \n");      
+        size_t leng = 0;
+        char *str = NULL;
+        while (getline(&str, &leng, stdin) != -1) {
+            c = strstr(str, argv[1]);
+            if (c) printf("%s", str);   
+        }
+
+        printf("\n");
+        return 0;
+    }
     for (size_t i = 2; i < argc; i++) {
-        int c;
+        //printf("opening file %s\n", argv[i]);
+        
         FILE *file;
         file = fopen(argv[i], "r");
         if (file) {
-            while ((c = getc(file)) != "\n")
-                putchar(c);
+            char *line = NULL;
+            size_t len = 0;
+            while(getline(&line, &len, file) != -1) {
+                c = strstr(line, argv[1]);
+                if (c) printf("%s", line);  
+            }
+            free(line);
+            printf("\n");
             fclose(file);
         }
         else {
