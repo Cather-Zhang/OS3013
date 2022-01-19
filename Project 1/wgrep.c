8748e47fc2485cd06235b4550f5bf24caf99c45e
Cather-Zhang <cather0215@gmail.com>
2022-01-19 17:45:01 +0800

    Update wgrep.c

--- Project 1/wgrep.c
+++ Project 1/wgrep.c
@@ -1,6 +1,23 @@
-int main(int argc, char const *argv[])
-{
-    /* code */
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+    if (argc == 1) printf("wgrep: searchterm [file ...]");
+    char* str = argv[1];
+    for (size_t i = 2; i < argc; i++) {
+        int c;
+        FILE *file;
+        file = fopen(argv[i], "r");
+        if (file) {
+            while ((c = getc(file)) != "\n")
+                putchar(c);
+            fclose(file);
+        }
+        else {
+            printf("wgrep: cannot open file\n");
+            return 1;   
+        }
+    }
     return 0;
 }
 
