70827d37671a2c1a0aefdb8b4567fa112228dda6
Cather-Zhang <cather0215@gmail.com>
2022-01-21 01:39:57 +0800

    passed 1 failed 2

--- Project 1/wzip.c
+++ Project 1/wzip.c
@@ -9,43 +9,53 @@ int main(int argc, char const *argv[]) {
         return 1;
     }
     else {
-        for (size_t i = 1; i < argc; i++) {
-            unsigned char c1, c2;
-            int count = 1;
-
-            FILE *in;
-            FILE *out;
-            FILE *read;
-            in = fopen(argv[i], "r");
-            out = fopen("result.txt","wb");
-            if (in) {
-
-                c1 = getc(in);
-                while (c1 != EOF) {
-                    if (c1 == (c2 = getc(in))) {
-                        count ++;
-                        c1 = c2;
-                    }
-                    else {
-                        printf("%d%c\n", count, c1);                    
-                        fwrite(&count,sizeof(count),1,out);
-                        fwrite(&c1,sizeof(c1),1,out);
-                        count = 1;
-                        c1 = c2;
-                    }
+        FILE *in;
+        if (argc > 2) {
+            int c;
+            FILE *each;
+            in = fopen(argv[1], "w");
+            for (size_t i = 2; i < argc; i++) {
+                each = fopen(argv[i], "r");
+                while ((c = getc(each)) != EOF) {
+                    fputc(c, in);
                 }
-                read = fopen("result.txt","rb");
-                while ((c = getc(read)) != EOF)
-                    putchar(c);
-                fclose(read);
-                fclose(out);
-                fclose(in);
             }
-            else {
-                printf("wzip: cannot open file\n");
-                return 1;   
-            }
+            fclose(in);
         }
+    
+        int c1, c2;
+        int count = 1;
+
+        FILE *out;
+        FILE *read;
+        in = fopen(argv[1], "r");
+        out = fopen("result.txt","wb");
+        if (in) {
+
+            c1 = getc(in);
+            while (c1 != EOF) {
+                if (c1 == (c2 = getc(in))) {
+                    count ++;
+                    c1 = c2;
+                }
+                else {
+                    putw(count, out);
+                    fputc(c1, out);
+                    count = 1;
+                    c1 = c2;
+                }
+            }
+            fclose(out);
+            read = fopen("result.txt","rb");
+            while ((c = getc(read)) != EOF)
+                putchar(c);
+            fclose(read);
+            fclose(in);
+        }
+        else {
+            printf("wzip: cannot open file\n");
+            return 1;   
+        }      
     }
     return 0;
 }
