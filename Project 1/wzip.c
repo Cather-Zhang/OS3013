d1b0d1c7463c5602025eee09b137f92bf261b0c1
Cather-Zhang <cather0215@gmail.com>
2022-01-21 01:11:11 +0800

    updated

--- Project 1/wzip.c
+++ Project 1/wzip.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-    printf("found %d arguments\n", argc);
+
     int c;
     if (argc == 1) {
         printf("wzip: File1 [File2 ...]\n");
@@ -10,13 +10,16 @@ int main(int argc, char const *argv[]) {
     }
     else {
         for (size_t i = 1; i < argc; i++) {
-            int c1, c2, count = 1;
+            unsigned char c1, c2;
+            int count = 1;
+
             FILE *in;
             FILE *out;
             FILE *read;
-            unsigned char buffer[5];
             in = fopen(argv[i], "r");
+            out = fopen("result.txt","wb");
             if (in) {
+
                 c1 = getc(in);
                 while (c1 != EOF) {
                     if (c1 == (c2 = getc(in))) {
@@ -24,14 +27,11 @@ int main(int argc, char const *argv[]) {
                         c1 = c2;
                     }
                     else {
-                        buffer[0] = count;
-                        buffer[1] = (count >> 8) & 0xFF;
-                        buffer[2] = (count >> 16) & 0xFF;
-                        buffer[3] = (count >> 24) & 0xFF;
-                        buffer[4] = c1;
-                        out = fopen("result.txt","wb");
-                        fwrite(buffer,sizeof(buffer),1,out);
+                        printf("%d%c\n", count, c1);                    
+                        fwrite(&count,sizeof(count),1,out);
+                        fwrite(&c1,sizeof(c1),1,out);
                         count = 1;
+                        c1 = c2;
                     }
                 }
                 read = fopen("result.txt","rb");
@@ -42,7 +42,7 @@ int main(int argc, char const *argv[]) {
                 fclose(in);
             }
             else {
-                printf("wcat: cannot open file\n");
+                printf("wzip: cannot open file\n");
                 return 1;   
             }
         }
