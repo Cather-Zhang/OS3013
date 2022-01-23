9471c3e6b938270d093d9bc41e4e97c12fd33844
Cather-Zhang <cather0215@gmail.com>
2022-01-23 13:49:06 +0800

    its not working

--- 2/wshell.c
+++ 2/wshell.c
@@ -1,1 +1,40 @@
-Place code here.
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <libgen.h>
+
+
+void type_prompt() {
+    char buffer[100];
+    char *path = getcwd(buffer, 100);
+    char *dir = basename(path);
+    printf("%s$ ", dir);
+}
+
+void read_command(char cmd[], char *par[]){
+
+}
+
+int main(int argc, char const *argv[])
+{
+    char cmd[100], command[100], *para[20];
+    char *envp[] = {(char *) "PATH=/bin", 0};
+    while (1) {
+        type_prompt();
+        read_command(command, para);
+        if (fork() != 0)
+            wait(NULL);
+        else {
+            strcat(cmd, command);
+            execve(cmd, para, envp);
+        }
+        if (strcmp(command, "exit") == 0)
+            break;
+    }
+    return 0;
+}
+
+
+
