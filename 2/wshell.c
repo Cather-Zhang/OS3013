3bed19dbb9f51a06c4c79c1a7d53d9c89228d19c
JaiiR320 <jair3202@gmail.com>
2022-01-24 00:37:03 -0500

    edit on wshell.c

--- 2/wshell.c
+++ 2/wshell.c
@@ -10,23 +10,61 @@ void type_prompt() {
     char buffer[100];
     char *path = getcwd(buffer, 100);
     char *dir = basename(path);
+    
     printf("%s$ ", dir);
+    fflush(stdout);
+    //return path;
 }
 
-void read_command(char cmd[], char *par[]){
+void read_command(char cmd[], char *par[]) {
+    char line[1024];
+    int count = 0, i = 0, j = 0;
+    char* array[100], *pch;
 
+    //read a line
+    for(;;){
+        int c = fgetc(stdin);
+        line[count++] = (char)c;
+        if(c == '\n') break;
+    }
+    if(count == 1) return;
+    pch = strtok(line, " \n");
+
+    while(pch != NULL){
+        array[i++] = strdup(pch);
+        pch = strtok(NULL, " \n");
+    }
+    //first word is command
+    strcpy(cmd, array[0]);
+
+    for(j = 0; j < i; j++){
+        par[j] = array[j];
+    }
+    par[i] = NULL;
 }
 
-int main(int argc, char const *argv[])
-{
+void print_cmd(char*command, char*para[]){
+    printf("running: %s\n", command);
+    for (size_t i = 0; i < 20; i++) {
+        if (para[i] == NULL) break;
+        printf("p%ld: %s\n", i, para[i]);
+    }
+}
+
+int main(int argc, char const *argv[]) {
     char cmd[100], command[100], *para[20];
     char *envp[] = {(char *) "PATH=/bin", 0};
+    //char* curr;
     while (1) {
+        //curr = 
         type_prompt();
         read_command(command, para);
+        chdir();
+        //printf("%s\n", curr);
         if (fork() != 0)
             wait(NULL);
         else {
+            strcpy(cmd, "/bin/");
             strcat(cmd, command);
             execve(cmd, para, envp);
         }
@@ -35,6 +73,4 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
-
-
 
