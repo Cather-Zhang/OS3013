266f436ba763f138037ecefc2ab2db9958c878cb
Cather-Zhang <cather0215@gmail.com>
2022-01-25 15:44:14 +0800

    part 2 done

--- 2/copy.c
+++ 2/copy.c
@@ -11,14 +11,11 @@ void read_command(char cmd[], char *par[]);
 
 int main(int argc, char const *argv[]) {
     char* curr = "";
-    char buffer[100];
-    char *home = getcwd(buffer, 100);
-    char *envp[] = {(char *) "PATH=/bin", 0};
     while (1) {
-        char command[100], cmd[100], *para[20];
+        char command[100], *para[20];//, *empty[20];
 
         curr = type_prompt();
-
+        //memcpy(para, empty, sizeof(empty));
         read_command(command, para);
 
         if(!isatty(fileno(stdin))){
@@ -32,52 +29,17 @@ int main(int argc, char const *argv[]) {
 
         //special case for cd
         if(strcmp(command, "cd") == 0){
-            if (para[1] == NULL) {
-                strcpy(curr, home);
-                chdir(curr);
-            } 
-            else if(para[2] != NULL) {
-                printf("wshell: cd: too many arguments\n");
-                fflush(stdout);
-            }
-            else{
-                strcpy(curr, para[1]);
-                if(chdir(curr) == -1)
-                    printf("wshell: no such directory: %s\n", curr);
-                para[1] = curr;
-            }
+            strcpy(curr, para[1]);
+            chdir(curr);
+            para[1] = curr;
         }
         //special case for pwd
-        else if(strcmp(command, "pwd") == 0){
+        if(strcmp(command, "pwd") == 0){
             char buffer[100];
             char *path = getcwd(buffer, 100);
             printf("%s\n", path);
             fflush(stdout);
-        } 
-        else if (strcmp(command, "echo") == 0) {
-            for (size_t i = 1; i < 20; i++) {
-                if (para[i] == NULL) break;
-                if (para[i+1] == NULL) printf("%s", para[i]);
-                else printf("%s ", para[i]);
-            }
-            printf("\n");
-        }
-             
-        else {
-            if (fork() != 0)
-                wait(NULL);
-            else {
-                char* c;
-                c = strstr(command, "/bin/");
-                if (!c)
-                    strcpy(cmd, "/bin/");
-                strcat(cmd, command);
-                if (execve(cmd, para, envp) == -1){
-                    printf("wshell: could not execute command: %s\n", command);
-                    break;
-                }
-            }
-        }  
+        }        
     }
     return 0;
 }
@@ -93,7 +55,7 @@ char* type_prompt() {
 }
 
 void read_command(char*cmd, char*par[]){
-    char line[1024] = "";
+    char line[100] = "";
     int count = 0;
     int i = 0;
 
@@ -102,6 +64,8 @@ void read_command(char*cmd, char*par[]){
         line[count++] = (char)c;
         c = fgetc(stdin);
     }
+
+    printf("\n");
     if(count == 1) return;
     char* p = strtok(line, " \n");
     while(p != NULL) {
@@ -120,7 +84,7 @@ void print_cmd(char*para[]){
     }
 
     for (size_t i = 1; i < 20; i++) {
-        if (para[i] == NULL) break;
+        if (para[i] == NULL) return;
         printf(" %s", para[i]);
     }
     printf("\n");
