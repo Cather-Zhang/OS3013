dc9734f1f25e92c1e4ba6fe76567167bb3d0bec7
Cather-Zhang <cather0215@gmail.com>
2022-01-26 00:52:14 +0800

    part 3 kinda

--- 2/wshell.c
+++ 2/wshell.c
@@ -7,19 +7,62 @@
 
 void print_cmd(char*para[]);
 char* type_prompt();
-void read_command(char cmd[], char *par[]);
+int read_command(char cmd[], char *par[], int ind, char *his[], char *queue[]);
+void print_his(char *his[], int index) ;
 
 int main(int argc, char const *argv[]) {
     char* curr = "";
     char buffer[100];
     char *home = getcwd(buffer, 100);
     char *envp[] = {(char *) "PATH=/bin", 0};
+    int rc;
+
+    int index = 0, isand = -1;
     while (1) {
-        char command[100], cmd[100], *para[20];
+        char command[20], cmd[20], *para[10], *history[10], *queue[10];
 
         curr = type_prompt();
-
-        read_command(command, para);
+        //printf("%s ", queue[0]);
+        //printf("%d \n", isand);
+        if (queue[0] != NULL) {
+            if ((isand == 1) ) {
+                printf("return code %d \n", rc);
+                isand = -1;
+                if (rc == 0) {
+                    printf("&& operation, rc = 0\n");
+                    strcpy(command, queue[0]);
+                    for (int i = 0; i < 10; i++) {
+                        if (queue[i] == NULL) break;
+                        else {
+                            strcpy(para[i], queue[i]);
+                            queue[i] = NULL;
+                        }       
+                    }
+                }
+                else {
+                    printf("&& operation, rc != 0\n");
+                    for (int i = 0; i < 10; i++) {
+                        queue[i] = NULL;
+                    }
+                    continue;     
+                }
+            }
+            else if ((isand == 0) && (rc == 0)) {
+                isand = -1;
+                strcpy(command, queue[0]);
+                for (int i = 0; i < 10; i++) {
+                    if (queue[i] != NULL){
+                        strcpy(para[i], queue[i]);
+                        queue[i] = NULL;
+                    }
+                }
+                continue;
+            }
+        }
+        else {
+            isand = read_command(command, para, index, history, queue);
+            index++;
+        }
 
         if(!isatty(fileno(stdin))){
             print_cmd(para);
@@ -45,6 +88,7 @@ int main(int argc, char const *argv[]) {
                 if(chdir(curr) == -1)
                     printf("wshell: no such directory: %s\n", curr);
                 para[1] = curr;
+                fflush(stdout);
             }
         }
         //special case for pwd
@@ -52,17 +96,34 @@ int main(int argc, char const *argv[]) {
             char buffer[100];
             char *path = getcwd(buffer, 100);
             printf("%s\n", path);
+            rc = 0;
             fflush(stdout);
         } 
         else if (strcmp(command, "echo") == 0) {
-            for (size_t i = 1; i < 20; i++) {
+            for (size_t i = 1; i < 10; i++) {
                 if (para[i] == NULL) break;
                 if (para[i+1] == NULL) printf("%s", para[i]);
                 else printf("%s ", para[i]);
             }
             printf("\n");
+            rc = 0;
+            fflush(stdout);
         }
-             
+        else if (strcmp(command, "history") == 0) {
+            if (index < 10) {
+                for (size_t i = 0; i < index; i++) {           
+                    printf("%s\n", history[i]);
+                }
+            }
+            else {
+                for (size_t i = index; i < index + 10; i++) {           
+                    size_t a = i % 10;
+                    printf("%s\n", history[a]);
+                }
+            }
+            fflush(stdout);
+        }
+        
         else {
             if (fork() != 0)
                 wait(NULL);
@@ -73,10 +134,12 @@ int main(int argc, char const *argv[]) {
                     strcpy(cmd, "/bin/");
                 strcat(cmd, command);
                 if (execve(cmd, para, envp) == -1){
+                    rc = -1;
                     printf("wshell: could not execute command: %s\n", command);
                     break;
                 }
             }
+            rc = 0;
         }  
     }
     return 0;
@@ -92,25 +155,40 @@ char* type_prompt() {
     return path;
 }
 
-void read_command(char*cmd, char*par[]){
+int read_command(char*cmd, char*par[], int index, char *his[], char *queue[]){
     char line[1024] = "";
     int count = 0;
-    int i = 0;
+    int i = 0, j = 0, isand;
 
     int c = fgetc(stdin);
     while(c != '\n'){
         line[count++] = (char)c;
         c = fgetc(stdin);
     }
-    if(count == 1) return;
+    if(count == 1) return -1;
+    his[index % 10] = strdup (line);
+    
     char* p = strtok(line, " \n");
-    while(p != NULL) {
-        par[i++] = strdup(p);
-        p = strtok(NULL, " \n");
+    while (p != NULL)
+    {
+        if ((strcmp(p, "&&") == 0) || (strcmp(p, "||") == 0)) {
+            if (strcmp(p, "&&") == 0) isand = 1;
+            else isand = 0;
+            p = strtok (NULL, " \n");
+            while (p != NULL) {
+                queue[j++] = strdup(p);
+                p = strtok (NULL, " \n");
+            }
+        }
+        else par[i++] = strdup(p);
+        p = strtok (NULL, " \n");
     }
     strcpy(cmd, par[0]);
     par[i] = NULL;
+    return isand;
 }
+
+
 
 void print_cmd(char*para[]){
     printf("%s", para[0]);
@@ -119,7 +197,7 @@ void print_cmd(char*para[]){
         return;
     }
 
-    for (size_t i = 1; i < 20; i++) {
+    for (size_t i = 1; i < 10; i++) {
         if (para[i] == NULL) break;
         printf(" %s", para[i]);
     }
--- video/2.1.c
+++ video/2.1.c
@@ -9,11 +9,17 @@ int main(int argc, char const *argv[]) {
     if (fork() != 0) 
         wait(NULL);
     else {
-        
-        //execl("/usr/bin/ls", "/usr/bin/ls", NULL);
+        execl("/usr/bin/ls", "ls", NULL);
         //execlp("ls", "ls", NULL);
-        char* arg[] = {"ls", NULL};
-        execvp(arg[0], arg);
-    }
+        //char* arg[] = {"ls", NULL};
+        //execvp(arg[0], arg)
     return 0;
+    }
 }
+
+
+
+        //execl("/usr/bin/ls", "/usr/bin/ls", NULL);
+               //execlp("ls", "ls", NULL);
+        //char* arg[] = {"ls", NULL};
+        //execvp(arg[0], arg);
