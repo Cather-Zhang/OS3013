747c515c7bf42eb1e28660d96df7fd8f58c9f08d
Cather-Zhang <cather0215@gmail.com>
2022-02-22 11:10:39 -0500

    project 5 works but no condition variable

--- 5/scan.c
+++ 5/scan.c
@@ -6,71 +6,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAX_LINE_SIZE 256
-
 
 void read_input_vector(const char* filename, int n, int* array);
 
-int *in, *out;
+int *in, *out, **end;
 int chunkSize;
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
 void* add(void* ptr) {
     pthread_mutex_lock(&mut);
     int index = *(int*)ptr;
-    printf("index: %d\n", index);
     out[index] = in[index];
     for (int i = 1; i < chunkSize; i++) {
         out[index + i] = out[index + i - 1]  + in[index + i];
     }
-    *(int*) ptr = out[index + chunkSize];
+    *(int*) ptr = out[index + chunkSize - 1];
     pthread_mutex_unlock(&mut);
     return (void*)ptr;
 }
 
 int main(int argc, char **argv) {
-    printf("start reading arguments\n");
     const char *filename = argv[1];
     int size = atoi(argv[2]);
     int threadNum = atoi(argv[3]);
     chunkSize = size / threadNum;
-    printf("chunk size is: %d\n", chunkSize);
-    printf("finished reading arguments\n");
 
     in = (int*) malloc(sizeof(int) * size);
     out = (int*) malloc(sizeof(int) * size);
     read_input_vector(filename, size, in);
 
-    printf("file name is: %s\n", filename);
-    printf("reading file...\n");
-    for (int i=0; i < size; i++) {
-        printf("%d\n", in[i]); 
-    } 
-    printf("size of array is: %d\n", size);
-    printf("number of threads to be generated is: %d\n", threadNum);
-
     pthread_t *thread = (pthread_t*) malloc(sizeof(pthread_t) * threadNum);
+    end = (int**) malloc(sizeof(int*) * threadNum);
     for (int i = 0; i < threadNum; i++) {
-        int* a = malloc(sizeof(int));
-        *a = i * chunkSize;
-        pthread_create(&thread[i], NULL, &add, a);
+        end[i] = (int*) malloc(sizeof(int));
+        *end[i] = i * chunkSize;
+        pthread_create(&thread[i], NULL, &add, end[i]);
     }
 
     for (int i = 0; i < threadNum; i++) {
         void *r;
         pthread_join(thread[i], &r);
         if (i != 0) {
-            for (int j = 0; j < chunkSize; j++)
-            out[i * chunkSize + 1] += (*(int*)r);
+            for (int j = 0; j < chunkSize; j++) {
+                int add = 0;
+                for (int k = 0; k < i; k++) 
+                    add += *(int*)end[k];
+                out[i * chunkSize + j] += add;
+            }
         }
     }
     for (int i = 0; i < size; i ++) {
         printf("%d\n", out[i]);
     }
 }
-
-
 
 void read_input_vector(const char* filename, int n, int* array)
 {
